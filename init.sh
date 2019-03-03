17460d5b579abef103a167104b221ce9f0f97db2
Steven Kalt <kalt.steven@gmail.com>
2019-03-03 16:58:08 -0500

    draft of variable logging

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
-hooks_dir="$(git rev-parse --git-dir)/hooks"
+git_dir="$(git rev-parse --git-dir)"
+hooks_dir="$git_dir/hooks"
 hooks=(
   "applypatch-msg" "pre-applypatch" "post-applypatch" "pre-commit"
   "prepare-commit-msg" "commit-msg" "post-commit" "pre-rebase" "post-checkout"
@@ -7,13 +8,20 @@ hooks=(
   "push-to-checkout" "pre-auto-gc" "post-rewrite" "sendemail-validate"
   "fsmonitor-watchman" "p4-pre-submit"
 )
+
 make-logger() {
   local hook=$1
   local hook_file="$hooks_dir/$hook"
-  echo $hook_file
-  echo "#! /usr/bin/env bash"  > $hook_file
-  echo "echo \"$hook\"" >> $hook_file
+  cat << EOF | sed 's/^    //' > $hook_file
+    #! /usr/bin/env bash
+    echo "$hook"
+    # ls-heads::in-dir
+    # ls-refs/**/*
+    # ls-vars
+    declare | grep -i 'git'
+EOF
   chmod +x $hook_file
+  echo $hook_file
 }
 
 main() {
