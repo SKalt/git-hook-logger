bf3de0babc6c604aa47d681f2338fdfb01db1896
Steven Kalt <kalt.steven@gmail.com>
2019-03-05 23:22:07 -0500

    removed deprecated utility fns file

--- src/utility-fns.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-local input="$1"; if [ -z "$input" ]; then input="$(cat -)"; fi
-
-function escape-quotes() {
-  local input="$1"; if [ -z "$input" ]; then input="$(cat -)"; fi
-  echo "\"$(echo $input | sed 's/"/\\"/g')\""
-}
