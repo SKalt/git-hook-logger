9cc9976e3fad0b308bb060a399d61f2e5f7f5dcf
Steven Kalt <kalt.steven@gmail.com>
2019-03-09 00:51:22 -0500

    that damn experiment

---
+++ experiments/simple-commit.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+DIR="${BASH_SOURCE[0]%/*}"
+source "$DIR/../init.sh" "simple-commit"
+main
+# steps to reproduce your desired state
+touch "$DIR/sandbox/foo.txt"
+git add "$DIR/sandbox/foo.txt"
+git commit -m 'test commit please ignore'
+rm $DIR/../.git/hooks/*
+# git reset HEAD^
